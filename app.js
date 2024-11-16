b6403afc987b57dbc5f395fc8ba36195ee9e2e90
unknown <jaydipchavda2216@gmail.com>
2024-11-16 17:18:23 +0530

    create first get request in notours app

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+const fs = require('fs');
 const express = require("express");
 
 const app = express();
 
-const port = 3000;
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+app.get('/api/v1/tours', (req,res)=>{
+    res.status(200).send({
+        status:'success',
+        results:tours.length,
+        data:{
+            tours
+        }
+    })
+});
+
+app.post('/api/v1/tours')
+const port = 3000;
 app.listen(port, ()=>{
-    console.log(`app running on port no ${port}`)
+    console.log(`app running on port no ${port}.....`)
 })
