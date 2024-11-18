ac1c3942d1023f01290b1281237d229e00c22902
unknown <jaydipchavda2216@gmail.com>
2024-11-18 12:09:13 +0530

    start building express app

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
-const fs = require('fs');
 const express = require("express");
-
+const morgan = require('morgan')
+const tourRouter = require('./dev-data/Routes/tourRoutes');
+const userRouter = require('./dev-data/Routes/userRoutes')
 const app = express();
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-app.get('/api/v1/tours', (req,res)=>{
-    res.status(200).send({
-        status:'success',
-        results:tours.length,
-        data:{
-            tours
-        }
-    })
-});
+// 1> middleware
+app.use(morgan('dev'))
 
-app.post('/api/v1/tours')
-const port = 3000;
-app.listen(port, ()=>{
-    console.log(`app running on port no ${port}.....`)
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`))
+
+// create own middleware
+app.use((req,res,next)=>{
+    console.log("hello from the middleware 👋👋");
+    next();
 })
+
+// create own middleware for time 
+app.use((req,res,next)=>{
+     req.requestTime = new Date().toISOString();
+    next();
+})
+
+
+// 2> ROUTE HANDLERS
+
+// 3> ROUTES
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+
+// app.get("/api/v1/tours/:id", getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+
+// 4> START THE SERVER
+
+module.exports = app;
