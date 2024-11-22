71c7b9d0c54999b3daf60de2f028ad740d0c684a
unknown <jaydipchavda2216@gmail.com>
2024-11-22 23:21:46 +0530

    tour model has been updated with more details

--- models/tourModel.js
+++ models/tourModel.js
@@ -11,14 +11,42 @@ const tourSchema = new mongoose.Schema({
         type:number,
         required:[true,'A tour must have duration']
     },
-    rating:{
+    maxGroupSize:{
+        type:number,
+        required:[true, 'A true must have a group size']
+    },
+    ratingAverage:{
         type: Number,
         default: 4.5
+    },
+    ratingsQuantity:{
+        type:number,
+        default:0
     },
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
-    }
+    },
+    priceDiscount:number,
+    summery:{
+        type: String,
+        trim: true,
+        required:[true, 'A tour must have a description']
+    },
+    description:{
+        type: String,
+        trim: true
+    },
+    imageCover:{
+        type:String,
+        required:[true, 'A tour must have a cover image']
+    },
+    image: [String],
+    createdAt:{
+        type: Date,
+        default:Date.now()
+    },
+    startDate: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
