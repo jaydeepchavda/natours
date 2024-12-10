a3217b7ccb19d90539b7c8937df83d7379002e46
unknown <jaydipchavda2216@gmail.com>
2024-12-11 00:01:38 +0530

    security messurement added in the natours app

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+const helmet = require("helmet")
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp')
+
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController'); 
 const tourRouter = require('./Routes/tourRoutes');
@@ -26,17 +32,47 @@ const app = express();
 // - 503 Service Unavailable: Service unavailable
 // - 504 Gateway Timeout: Gateway Timeout
 
-// 1> middleware
+
+
+// 1> Global middleware
+// security http headers 
+app.use(helmet());
+
+
+// development loggin 
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
-
 }
 
-app.use(express.json());
+// http limit request for same api 
+const limiter = rateLimit({
+    max:100,
+    windowMs:60*60*1000,
+    message:"Too many request from this IP, please try again in an hour !.."
+});
+// put rate limit for too many request 
+app.use("/api",limiter);
 
+// Body parser, reading data from body  into req.body 
+app.use(express.json( { limit : '10kb' }));
+
+// data sanitizarion  against NoSQL query injection 
+app.use(mongoSanitize());
+
+// data sanitizarion  against XSS 
+app.use(xss())
+
+// Prevent parameter pollution
+app.use(hpp({
+    whitelist: [ 'duration' , 'ratingAverage', 'ratingsQuantity','maxGroupSize','difficulty','price']
+}));
+
+
+
+// serving static file 
 app.use(express.static(`${__dirname}/public`))
 
-// create own middleware
+// create own middleware testing middle ware
 // app.use((req,res,next)=>{
 //     console.log("hello from the middleware 👋👋");
 //     next();
