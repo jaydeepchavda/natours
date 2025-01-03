b15d5f161540eaeab9a4a95bf92f0e3a5b773a68
unknown <jaydipchavda2216@gmail.com>
2025-01-03 23:04:58 +0530

    import users and tours data in natours app

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const Tour = require('./../../models/tourModel');
+const User = require('./../../models/userModel');
+const Review = require('./../../models/reviewModel');
+
 
 dotenv.config({path:'./config.env'})
  
@@ -19,22 +22,32 @@ mongoose
 
 // read json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`,'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`,'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`,'utf-8'));
+
 
 // import data into db
 
 const importData = async () => {
     try{
         await Tour.create(tours);
+        await User.create(users, { validateBeforeSave:false }) ;
+        await Review.create(reviews);
         console.log('Data successfully leaded!');
+
     } catch (err) {
         console.log(err);
+
     }
     process.exit();
 }
 
 const deleteData = async () => { 
     try{
-        await Tour.deleteMany()
+        await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
+
         console.log('Data successfully deleted!')
     } catch (err) {
         console.log(err);
