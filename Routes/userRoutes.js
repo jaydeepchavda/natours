a3add996d3a2b58f95e6758bd7ff727941c8ca6d
unknown <jaydipchavda2216@gmail.com>
2025-01-20 22:08:00 +0530

    issue solved

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
-// route
 const router = express.Router();
 
 router.post('/signup', authController.signup);
